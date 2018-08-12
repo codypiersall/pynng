4e4d8293d8e5b90b2826d01e5b82ceaeb13a1e29
Cody Piersall <cody.piersall@gmail.com>
2018-08-12 14:37:17 -0500

    Add support for Windows.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-cffi bindings for Nanomsg
+Python bindings for Nanomsg, using cffi
 
 TODO
 ====
@@ -6,6 +6,7 @@ TODO
 * Right now I'm modifying the CMake build file to set
   `CMAKE_POSITION_INDEPENDENT_CODE`.  This avoids problems when linking into
   the final .so file, but there is probably a better way to do it.
+  Additionally, I only do this on Linux; on Windows I leave CMake alone.
 * Generating the API file is fragile, becuase it is not remotely C syntax
   aware; it's just dumb old sed.
-
+* Do the right thing for 32/64 bit Python on Windows.
--- build.py
+++ build.py
@@ -3,21 +3,32 @@
 # this is to avoid confusion with the other 'ffi' object you get below
 
 from cffi import FFI
+import sys
 import subprocess
 ffibuilder = FFI()
+
+if sys.platform == 'win32':
+    objects = ['./nng/build/Debug/nng.lib']
+    # libraries determined to be necessary through trial and error
+    libraries = ['Ws2_32', 'Advapi32']
+else:
+    objects = ['./nng/build/libnng.a']
+    libraries = []
+
 
 ffibuilder.set_source("_nng",
    r""" // passed to the real C compiler,
         // contains implementation of things declared in cdef()
+        #define NNG_DECL
         #include <nng.h>
         #include <protocol/pair1/pair.h>
 
     """,
-    # libraries=['nng'],
-    # library_dirs=['build',],
+    libraries=libraries,
+    # library_dirs=['nng/build/Debug',],
     # (more arguments like setup.py's Extension class:
     include_dirs=['nng/src'],
-    extra_objects=['./nng/build/libnng.a'],
+    extra_objects=objects,
 )
 
 with open('nng_api.h') as f:
