308474891b9dddd7c5278b3949c0afdafa5aecf5
Cody Piersall <cody.piersall@gmail.com>
2018-08-12 21:30:42 -0500

    TEST

---
+++ test/test_bindings.py
@@ -0,0 +1,75 @@
+import pynng as nng
+import pytest
+import time
+
+# TODO: all sockets need timeouts
+
+
+addr = 'tcp://127.0.0.1:13131'
+
+
+def test_pair0():
+    s0 = nng.Pair0(listen=addr)
+    s1 = nng.Pair0(dial=addr)
+
+    val = b'oaisdjfa'
+    s1.send(val)
+    assert s0.recv() == val
+    s0.close()
+    s1.close()
+
+
+def test_pair1():
+    s0 = nng.Pair1(listen=addr)
+    s1 = nng.Pair1(dial=addr)
+
+    val = b'oaisdjfa'
+    s1.send(val)
+    assert s0.recv() == val
+    s0.close()
+    s1.close()
+
+
+def test_reqrep0():
+    req = nng.Req0(listen=addr)
+    rep = nng.Rep0(dial=addr)
+
+    request = b'i am requesting'
+    req.send(request)
+    assert rep.recv() == request
+
+    response = b'i am responding'
+    rep.send(response)
+    assert req.recv() == response
+
+    # TODO: when changing exceptions elsewhere, change here!
+    # requesters can't receive before sending
+    with pytest.raises(Exception):
+        req.recv()
+
+    # responders can't send before receiving
+    with pytest.raises(Exception):
+        rep.send()
+    req.close()
+    rep.close()
+
+
+def test_pubsub0():
+    sub = nng.Sub0(listen=addr)
+    pub = nng.Pub0(dial=addr)
+
+    request = b'i am requesting'
+    time.sleep(0.11)
+    pub.send(request)
+    assert sub.recv() == request
+
+    # TODO: when changing exceptions elsewhere, change here!
+    # publishers can't recv
+    with pytest.raises(Exception):
+        pub.recv()
+
+    # responders can't send before receiving
+    with pytest.raises(Exception):
+        sub.send()
+    pub.close()
+    sub.close()
