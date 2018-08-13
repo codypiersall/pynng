330722f3558eef2ae14fd3b255cd12672eae558d
Cody Piersall <cody.piersall@gmail.com>
2018-08-12 20:59:06 -0500

    Add all the sockets to the Python interface.

--- pynng/nng.py
+++ pynng/nng.py
@@ -6,6 +6,18 @@ Provides a Pythonic interface to cffi nng bindings
 from ._nng import ffi, lib
 
 
+__all__ = '''
+ffi lib
+Bus0
+Pair0
+Pair1
+Pull0 Push0
+Pub0 Sub0
+Req0 Rep0
+Surveyor0 Respondent0
+'''.split()
+
+
 def convert_address(addr):
     """Convert str or bytes to char*."""
     if isinstance(addr, str):
@@ -13,6 +25,11 @@ def convert_address(addr):
     if not isinstance(addr, ffi.CData):
         addr = ffi.new('char[]', addr)
     return addr
+
+
+def check_open(ret_val):
+    if ret_val:
+        raise Exception('TODO: Better exception')
 
 
 class Socket:
@@ -38,7 +55,6 @@ class Socket:
         if ret:
             raise Exception('TODO: better exception')
 
-
     def close(self):
         lib.nng_close(self.socket)
 
@@ -47,7 +63,7 @@ class Socket:
 
     @property
     def socket(self):
-         return self._socket_pointer[0]
+        return self._socket_pointer[0]
 
     def recv(self):
         """recv() on the socket.  Allows the nanomsg library to allocate and
@@ -71,12 +87,80 @@ class Socket:
         lib.nng_send(self.socket, data, len(data), 0)
 
 
-class Pair1Socket(Socket):
-    """A Pair v1 socket."""
+class Bus0(Socket):
+    """A bus0 socket"""
     def __init__(self):
         super().__init__()
-        if lib.nng_pair1_open(self._socket_pointer):
-            raise Exception('TODO: better exception')
+        check_open(lib.nng_bus0_open(self._socket_pointer))
 
 
-PairSocket = Pair1Socket
+class Pair0(Socket):
+    """A pair0 socket."""
+    def __init__(self):
+        super().__init__()
+        check_open(lib.nng_pair0_open(self._socket_pointer))
+
+
+class Pair1(Socket):
+    """A pair1 socket."""
+    def __init__(self):
+        super().__init__()
+        check_open(lib.nng_pair1_open(self._socket_pointer))
+
+
+class Pull0(Socket):
+    """A pull0 socket."""
+    def __init__(self):
+        super().__init__()
+        check_open(lib.nng_pull0_open(self._socket_pointer))
+
+
+class Push0(Socket):
+    """A push0 socket."""
+    def __init__(self):
+        super().__init__()
+        check_open(lib.nng_push0_open(self._socket_pointer))
+
+
+class Pub0(Socket):
+    """A Pub0 socket."""
+    def __init__(self):
+        super().__init__()
+        check_open(lib.nng_pub0_open(self._socket_pointer))
+
+
+class Sub0(Socket):
+    """A Sub0 socket."""
+    def __init__(self):
+        super().__init__()
+        check_open(lib.nng_sub0_open(self._socket_pointer))
+
+
+class Req0(Socket):
+    """A Req0 socket."""
+    def __init__(self):
+        super().__init__()
+        check_open(lib.nng_req0_open(self._socket_pointer))
+
+
+class Rep0(Socket):
+    """A Rep0 socket."""
+    def __init__(self):
+        super().__init__()
+        check_open(lib.nng_rep0_open(self._socket_pointer))
+
+
+class Respondent0(Socket):
+    """A respondent0 socket."""
+    def __init__(self):
+        super().__init__()
+        check_open(lib.nng_respondent0_open(self._socket_pointer))
+
+
+class Surveyor0(Socket):
+    """A surveyor0 socket."""
+    def __init__(self):
+        super().__init__()
+        check_open(lib.nng_surveyor0_open(self._socket_pointer))
+
+
