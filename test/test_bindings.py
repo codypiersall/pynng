7f35fd1e2f4b03104735c8e48645241c39a3794f
Cody Piersall <cody.piersall@gmail.com>
2018-08-14 09:39:22 -0500

    Use context managers in all tests.
    Using context managers ensures that each test cleans up after itself,
    so we can reuse the same address over and over.

--- test/test_bindings.py
+++ test/test_bindings.py
@@ -8,79 +8,78 @@ import time
 addr = 'tcp://127.0.0.1:13131'
 
 
-def test_can_do_timeout():
-    s0 = nng.Pair0(listen=addr)
-    # default is -1
-    assert s0.recv_timeout == -1
-    s0.recv_timeout = 1  # 1 ms, not too long
-    with pytest.raises(Exception):  # TODO: better exception
-        s0.recv()
-    s0.close()
+def test_context_manager_works():
+    # we have to grab a reference to the sockets so garbage collection doesn't
+    # close the socket for us automatically.
+    with nng.Pair0(listen=addr) as s0:  # noqa
+        pass
+    # we should be able to do it again if the context manager worked
+    with nng.Pair0(listen=addr) as s1:  # noqa
+        pass
+
+
+def test_timeout_works():
+    with nng.Pair0(listen=addr) as s0:
+        # default is -1
+        assert s0.recv_timeout == -1
+        s0.recv_timeout = 1  # 1 ms, not too long
+        with pytest.raises(Exception):  # TODO: better exception
+            s0.recv()
 
 
 def test_pair0():
-    s0 = nng.Pair0(listen=addr, recv_timeout=100)
-    s1 = nng.Pair0(dial=addr, recv_timeout=100)
-
-    val = b'oaisdjfa'
-    s1.send(val)
-    assert s0.recv() == val
-    s0.close()
-    s1.close()
+    with nng.Pair0(listen=addr, recv_timeout=100) as s0, \
+            nng.Pair0(dial=addr, recv_timeout=100) as s1:
+        val = b'oaisdjfa'
+        s1.send(val)
+        assert s0.recv() == val
 
 
 def test_pair1():
-    s0 = nng.Pair1(listen=addr, recv_timeout=100)
-    s1 = nng.Pair1(dial=addr, recv_timeout=100)
-
-    val = b'oaisdjfa'
-    s1.send(val)
-    assert s0.recv() == val
-    s0.close()
-    s1.close()
+    with nng.Pair1(listen=addr, recv_timeout=100) as s0, \
+            nng.Pair1(dial=addr, recv_timeout=100) as s1:
+        val = b'oaisdjfa'
+        s1.send(val)
+        assert s0.recv() == val
 
 
 def test_reqrep0():
-    req = nng.Req0(listen=addr, recv_timeout=100)
-    rep = nng.Rep0(dial=addr, recv_timeout=100)
+    with nng.Req0(listen=addr, recv_timeout=100) as req, \
+            nng.Rep0(dial=addr, recv_timeout=100) as rep:
 
-    request = b'i am requesting'
-    req.send(request)
-    assert rep.recv() == request
+        request = b'i am requesting'
+        req.send(request)
+        assert rep.recv() == request
 
-    response = b'i am responding'
-    rep.send(response)
-    assert req.recv() == response
+        response = b'i am responding'
+        rep.send(response)
+        assert req.recv() == response
 
-    # TODO: when changing exceptions elsewhere, change here!
-    # requesters can't receive before sending
-    with pytest.raises(Exception):
-        req.recv()
+        # TODO: when changing exceptions elsewhere, change here!
+        # requesters can't receive before sending
+        with pytest.raises(Exception):
+            req.recv()
 
-    # responders can't send before receiving
-    with pytest.raises(Exception):
-        rep.send()
-    req.close()
-    rep.close()
+        # responders can't send before receiving
+        with pytest.raises(Exception):
+            rep.send()
 
 
 def test_pubsub0():
-    sub = nng.Sub0(listen=addr, recv_timeout=100)
-    sub.subscribe(b'')
-    pub = nng.Pub0(dial=addr, recv_timeout=100)
+    with nng.Sub0(listen=addr, recv_timeout=100) as sub, \
+            nng.Pub0(dial=addr, recv_timeout=100) as pub:
+        sub.subscribe(b'')
 
-    request = b'i am requesting'
-    time.sleep(0.01)
-    pub.send(request)
-    assert sub.recv() == request
+        request = b'i am requesting'
+        time.sleep(0.01)
+        pub.send(request)
+        assert sub.recv() == request
 
-    # TODO: when changing exceptions elsewhere, change here!
-    # publishers can't recv
-    with pytest.raises(Exception):
-        pub.recv()
+        # TODO: when changing exceptions elsewhere, change here!
+        # publishers can't recv
+        with pytest.raises(Exception):
+            pub.recv()
 
-    # responders can't send before receiving
-    with pytest.raises(Exception):
-        sub.send()
-    pub.close()
-    sub.close()
+        # responders can't send before receiving
+        with pytest.raises(Exception):
+            sub.send()
