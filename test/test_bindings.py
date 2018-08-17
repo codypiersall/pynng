0095b23c08a9d1f7275b9ddebfed7c2d077501c9
Cody Piersall <cody.piersall@gmail.com>
2018-08-16 22:05:23 -0500

    Add push/pull and survey/respond tests.

--- test/test_bindings.py
+++ test/test_bindings.py
@@ -1,6 +1,7 @@
 import pynng as nng
 import pytest
 import time
+import threading
 
 # TODO: all sockets need timeouts
 
@@ -66,12 +67,12 @@ def test_reqrep0():
 def test_pubsub0():
     with nng.Sub0(listen=addr, recv_timeout=100) as sub, \
             nng.Pub0(dial=addr, recv_timeout=100) as pub:
-        sub.subscribe(b'')
 
-        request = b'i am requesting'
+        sub.subscribe(b'')
+        msg = b'i am requesting'
         time.sleep(0.01)
-        pub.send(request)
-        assert sub.recv() == request
+        pub.send(msg)
+        assert sub.recv() == msg
 
         # TODO: when changing exceptions elsewhere, change here!
         # publishers can't recv
@@ -86,6 +87,67 @@ def test_pubsub0():
                      """)
             # alas, it was not meant to be, subscriber.  Not every socket was
             # meant to publish.
+
+
+def test_push_pull():
+    received = {'pull1': False, 'pull2': False}
+    with nng.Push0(listen=addr) as push, \
+            nng.Pull0(dial=addr, recv_timeout=1000) as pull1, \
+            nng.Pull0(dial=addr, recv_timeout=1000) as pull2:
+
+        def recv1():
+            pull1.recv()
+            received['pull1'] = True
+
+        def recv2():
+            pull2.recv()
+            received['pull2'] = True
+
+        # push/pull does round robin style distribution
+        t1 = threading.Thread(target=recv1, daemon=True)
+        t2 = threading.Thread(target=recv2, daemon=True)
+
+        t1.start()
+        t2.start()
+        push.send(b'somewhere someone should see this')
+        push.send(b'somewhereeeee')
+        t1.join()
+        t2.join()
+        assert received['pull1']
+        assert received['pull2']
+
+
+def test_surveyor_respondent():
+    with nng.Surveyor0(listen=addr, recv_timeout=1000) as surveyor, \
+            nng.Respondent0(dial=addr, recv_timeout=1000) as resp1, \
+            nng.Respondent0(dial=addr, recv_timeout=2000) as resp2:
+        query = b"hey how's it going buddy?"
+        # wait for sockets to connect
+        time.sleep(0.01)
+        surveyor.send(query)
+        assert resp1.recv() == query
+        assert resp2.recv() == query
+        resp1.send(b'not too bad I suppose')
+
+        msg2 = b'''
+            Thanks for asking.  It's been a while since I've had
+            human contact; times have been difficult for me.  I woke up this
+            morning and again could not find a pair of matching socks.  I know that
+            a lot of people think it's worth it to just throw all your old socks
+            out and buy like 12 pairs of identical socks, but that just seems so
+            mundane.  Life is about more than socks, you know?  So anyway, since I
+            couldn't find any socks, I went ahead and put banana peels on my
+            feet.  They don't match *perfectly* but it's close enough.  Anyway
+            thanks for asking, I guess I'm doing pretty good.
+        '''
+        resp2.send(msg2)
+        resp = [surveyor.recv() for _ in range(2)]
+        assert b'not too bad I suppose' in resp
+        # if they're not the same
+        assert msg2 in resp
+
+        with pytest.raises(nng.exceptions.BadState):
+            resp2.send(b'oadsfji')
 
 
 def test_cannot_instantiate_socket_without_opener():
