326a0667f50551679cf4911a92ca39b4ecc70b7a
Cody Piersall <cody.piersall@gmail.com>
2018-08-20 22:22:20 -0500

    Add more options.

--- pynng/nng.py
+++ pynng/nng.py
@@ -65,6 +65,12 @@ class MsOption(_NNGOption):
     _setter = '_setopt_ms'
 
 
+class SizeOption(_NNGOption):
+    """Descriptor for getting/setting size_t options"""
+    _getter = '_getopt_size'
+    _setter = '_setopt_size'
+
+
 class StringOption(_NNGOption):
     """Descriptor for getting/setting string options"""
     _getter = '_getopt_string'
@@ -91,31 +97,117 @@ class NotImplementedOption(_NNGOption):
 
 
 class Socket:
-    """The base socket.  It should not be instantiated directly."""
+    """
+    The base socket.  It should generally not be instantiated directly.
+    See the documentation for __init__ for initialization options.
+
+    Sockets have the following attributes:
+
+    Attributes:
+        name (str): The socket name.  Corresponds to ``NNG_OPT_SOCKNAME``.
+            This is for debugging purposes.
+        raw (bool): A boolean, indicating whether the socket is raw or cooked.
+            Returns True if the socket is raw, else False.  This property is
+            read-only.  Corresponds to library option ``NNG_OPT_RAW``.  For
+            more information see
+            https://nanomsg.github.io/nng/man/v1.0.1/nng.7.html#raw_mode.
+        protocol (int): Read-only option which returns the 16-bit number of the
+            socket's protocol.
+        protocol_name (str): Read-only option which returns the name of the
+            socket's protocol.
+        peer (int): Returns the peer protocol id for the socket.
+        recv_timeout (int): Receive timeout, in ms.  If a socket takes longer
+            than the specified time, raises a pynng.exceptions.Timeout.
+            Corresponds to library option``NNG_OPT_RECVTIMEO`` *
+        send_timeout (int): Send timeout, in ms.  If the message cannot be
+            queued in the specified time, raises a pynng.exceptions.Timeout.
+            Corresponds to library option ``NNG_OPT_SENDTIMEO``.
+        local_address: Not implemented!!!  A read-only property representing
+            the local address used for communication.  The Python wrapper for
+            [nng_sockaddr](https://nanomsg.github.io/nng/man/v1.0.1/nng_sockaddr.5.html)
+            needs to be completed first.  Corresponds to ``NNG_OPT_LOCADDR``.
+        reconnect_time_min (int): The minimum time to wait before attempting
+            reconnects, in ms.  Corresponds to ``NNG_OPT_RECONNMINT``.
+        reconnect_time_max (int): The maximum time to wait before attempting
+            reconnects, in ms.  Corresponds to ``NNG_OPT_RECONNMAXT``.  If this
+            is non-zero, then the time between successive connection attempts
+            will start at the value of reconnect_time_min, and grow
+            exponentially, until it reaches this value.
+
+    See also the nng man pages document for options:
+
+    https://nanomsg.github.io/nng/man/v1.0.1/nng_options.5.html
+    """
+
+    # the following options correspond to nng options documented at
+    # https://nanomsg.github.io/nng/man/v1.0.1/nng_options.5.html
+    name = StringOption('socket-name')
+    raw = BooleanOption('raw')
+    protocol = IntOption('protocol')
+    protocol_name = StringOption('protocol-name')
+    peer = IntOption('peer')
+    peer_name = StringOption('peer-name')
+    recv_buffer_size = IntOption('recv-buffer')
+    send_buffer_size = IntOption('send-buffer')
+    recv_timeout = MsOption('recv-timeout')
+    send_timeout = MsOption('send-timeout')
+    # TODO: local_address and rmeote_address are dialer/connected pipe options,
+    # NOT socket options.
+    local_address = NotImplementedOption(
+        'local-address',
+        'NNG_OPT_LOCADDR requires a Python wrapper for nng_sockaddr, which '
+        'has not been done yet.'
+    )
+    remote_address = NotImplementedOption(
+        'peer-address',
+        'NNG_OPT_REMADDR requires a Python wrapper for nng_sockaddr, which '
+        'has not been done yet.'
+    )
+    # TODO: url belongs on listner/dialers, not socket
+    url = StringOption('url')
+    # TODO: does this belong here?
+    ttl_max = IntOption('ttl-max')
+    recv_max_size = SizeOption('recv-size-max')
+    reconnect_time_min = MsOption('reconnect-time-min')
+    reconnect_time_max = MsOption('reconnect-time-max')
 
     def __init__(self, *,
-                 opener=None,
                  dial=None,
                  listen=None,
                  recv_timeout=None,
-                 send_timeout=None
+                 send_timeout=None,
+                 recv_buffer_size=None,
+                 send_buffer_size=None,
+                 recv_max_size=None,
+                 reconnect_time_min=None,
+                 reconnect_time_max=None,
+                 opener=None
                  ):
-        """Initialize socket.
-        ``opener`` is the function in the ffi bindings used to initialize the
-        specific socket.  For example, for a Pair1 socket, the function is
-        ``ffi.nng_pair1_open``
-        If ``dial`` is not None, attempt to dial the specified address.
-        If ``listen`` is not None, attempt to listen at the specified address.
-        If ``recv_timeout`` is not None, it is the receive timeout in ms.
-        If ``send_timeout`` is not None, it is the receive timeout in ms.
+        """Initialize socket.  It takes no positional arguments.
+        Most socket options can be set through the initializer for convenience.
+        the keyword function
+
+        Note:
+            The following arguments are all optional.
+
+        Args:
+            dial (str): The address to dial.  If not given, no address is
+                dialed.
+            listen (str): The address to listen at.  If not given, the socket
+                does not listen at any address.
+            recv_timeout: The receive timeout, in milliseconds.  If not given,
+                there is no timeout.
+            send_timeout: The send timeout, in milliseconds.  If not given,
+                there is no timeout.
+            recv_buffer_size: is not None, it sets receive message buffer size.
 
         """
         self._socket_pointer = ffi.new('nng_socket[]', 1)
         if opener is not None:
-            self.opener = opener
-        if opener is None and not hasattr(self, 'opener'):
+            self._opener = opener
+        if opener is None and not hasattr(self, '_opener'):
             raise TypeError('Cannot directly instantiate a Socket.  Try a subclass.')
-        check_err(self.opener(self._socket_pointer))
+        check_err(self._opener(self._socket_pointer))
         if dial is not None:
             self.dial(dial)
         if listen is not None:
@@ -167,6 +259,15 @@ class Socket:
         err = nng.nng_send(self.socket, data, len(data), 0)
         check_err(err)
 
+    def _getopt_int(self, option):
+        """Gets the specified option"""
+        i = ffi.new('int []', 1)
+        opt_as_char = to_char(option)
+        # attempt to accept floats that are exactly int
+        ret = nng.nng_getopt_int(self.socket, opt_as_char, i)
+        check_err(ret)
+        return i[0]
+
     def _setopt_int(self, option, value):
         """Sets the specified option to the specified value"""
         opt_as_char = to_char(option)
@@ -178,14 +279,33 @@ class Socket:
         value = int(value)
         nng.nng_setopt_int(self.socket, opt_as_char, value)
 
-    def _getopt_int(self, option):
-        """Gets the specified option"""
-        i = ffi.new('int []', 1)
+    def _getopt_size(self, option):
+        """Gets the specified size option"""
+        i = ffi.new('size_t []', 1)
         opt_as_char = to_char(option)
         # attempt to accept floats that are exactly int
-        ret = nng.nng_getopt_int(self.socket, opt_as_char, i)
+        ret = nng.nng_getopt_size(self.socket, opt_as_char, i)
         check_err(ret)
         return i[0]
+
+    def _setopt_size(self, option, value):
+        """Sets the specified size option to the specified value"""
+        opt_as_char = to_char(option)
+        # attempt to accept floats that are exactly int
+        if not int(value) == value:
+            msg = 'Invalid value {} of type {}.  Expected int.'
+            msg = msg.format(value, type(value))
+            raise ValueError(msg)
+        value = int(value)
+        nng.nng_setopt_size(self.socket, opt_as_char, value)
+
+    def _getopt_ms(self, option):
+        """Gets the specified option"""
+        ms = ffi.new('nng_duration []', 1)
+        opt_as_char = to_char(option)
+        ret = nng.nng_getopt_ms(self.socket, opt_as_char, ms)
+        check_err(ret)
+        return ms[0]
 
     def _setopt_ms(self, option, value):
         """Sets the specified option to the specified value"""
@@ -199,13 +319,15 @@ class Socket:
         value = int(value)
         nng.nng_setopt_ms(self.socket, opt_as_char, value)
 
-    def _getopt_ms(self, option):
-        """Gets the specified option"""
-        ms = ffi.new('nng_duration []', 1)
+    def _getopt_string(self, option):
+        """Gets the specified string option"""
+        opt = ffi.new('char *[]', 1)
         opt_as_char = to_char(option)
-        ret = nng.nng_getopt_ms(self.socket, opt_as_char, ms)
+        ret = nng.nng_getopt_string(self.socket, opt_as_char, opt)
         check_err(ret)
-        return ms[0]
+        py_obj = ffi.string(opt[0]).decode()
+        nng.nng_strfree(opt[0])
+        return py_obj
 
     def _setopt_string(self, option, value):
         """Sets the specified option to the specified value
@@ -218,14 +340,6 @@ class Socket:
         ret = nng.nng_setopt(self.socket, opt_as_char, val_as_char, len(value))
         check_err(ret)
 
-    def _getopt_string(self, option):
-        """Gets the specified string option"""
-        opt = ffi.new('char *[]', 1)
-        opt_as_char = to_char(option)
-        ret = nng.nng_getopt_string(self.socket, opt_as_char, opt)
-        check_err(ret)
-        return ffi.string(opt[0]).decode()
-
     def _getopt_bool(self, option):
         """Return the boolean value of the specified option"""
         opt_as_char = to_char(option)
@@ -235,7 +349,7 @@ class Socket:
         return b[0]
 
     def _setopt_bool(self, option, value):
-        """Sets the specified option to the specified value"""
+        """Sets the specified option to the specified value."""
         opt_as_char = to_char(option)
         ret = nng.nng_setopt_bool(self.socket, opt_as_char, value)
         check_err(ret)
@@ -246,45 +360,41 @@ class Socket:
     def __exit__(self, *tb_info):
         self.close()
 
-    recv_timeout = MsOption('recv-timeout')
-    send_timeout = MsOption('send-timeout')
-    name = StringOption('socket-name')
-    raw = BooleanOption('raw')
-
 
 class Bus0(Socket):
-    """A bus0 socket"""
-    opener = nng.nng_bus0_open
+    """A bus0 socket."""
+
+    _opener = nng.nng_bus0_open
 
 
 class Pair0(Socket):
     """A pair0 socket."""
-    opener = nng.nng_pair0_open
+    _opener = nng.nng_pair0_open
 
 
 class Pair1(Socket):
     """A pair1 socket."""
-    opener = nng.nng_pair1_open
+    _opener = nng.nng_pair1_open
 
 
 class Pull0(Socket):
     """A pull0 socket."""
-    opener = nng.nng_pull0_open
+    _opener = nng.nng_pull0_open
 
 
 class Push0(Socket):
     """A push0 socket."""
-    opener = nng.nng_push0_open
+    _opener = nng.nng_push0_open
 
 
 class Pub0(Socket):
     """A pub0 socket."""
-    opener = nng.nng_pub0_open
+    _opener = nng.nng_pub0_open
 
 
 class Sub0(Socket):
     """A Sub0 socket."""
-    opener = nng.nng_sub0_open
+    _opener = nng.nng_sub0_open
 
     def subscribe(self, topic):
         """Subscribe to the specified topic."""
@@ -297,22 +407,22 @@ class Sub0(Socket):
 
 class Req0(Socket):
     """A Req0 socket."""
-    opener = nng.nng_req0_open
+    _opener = nng.nng_req0_open
 
 
 class Rep0(Socket):
     """A Rep0 socket."""
-    opener = nng.nng_rep0_open
+    _opener = nng.nng_rep0_open
 
 
 class Surveyor0(Socket):
     """A surveyor0 socket."""
-    opener = nng.nng_surveyor0_open
+    _opener = nng.nng_surveyor0_open
 
 
 class Respondent0(Socket):
     """A respondent0 socket."""
-    opener = nng.nng_respondent0_open
+    _opener = nng.nng_respondent0_open
 
 
 
