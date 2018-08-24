6334684f8b021da788e7f7b4046e49b55fa76880
Cody Piersall <cody.piersall@gmail.com>
2018-08-23 22:58:00 -0500

    Get pynng to successfully build on Linux via setup.py.  Yay!

--- setup.py
+++ setup.py
@@ -1,9 +1,46 @@
-import setuptools
+import build_pynng
+import os
+import subprocess
+import sys
+
+import setuptools.command.build_py
+
+
+THIS_DIR = os.path.dirname(__file__)
+
+
+def build_nng_lib():
+    if os.path.exists(build_pynng.objects[0]):
+        # the object file we were planning on building already exists; we'll
+        # just use it!
+        return
+    if sys.platform == 'win32':
+        # pick the correct cmake generator
+        is_64bit = sys.maxsize > 2**32
+        cmd = [os.path.join(THIS_DIR, 'build_nng.bat')]
+    else:
+        script = os.path.join(THIS_DIR, 'build_nng.sh')
+        cmd = ['/bin/bash', script]
+
+    subprocess.check_call(cmd)
+
+# TODO: this is basically a hack to get something to run before running cffi
+# extnsion builder. subclassing something else would be better!
+class BuildCommand(setuptools.command.build_py.build_py):
+    """Custom build command."""
+
+    def run(self):
+        build_nng_lib()
+        super(BuildCommand, self).run()
+
 
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
 setuptools.setup(
+    cmdclass={
+        'build_py': BuildCommand,
+    },
     name='pynng',
     version='0.1.0-pre',
     author='Cody Piersall',
@@ -24,7 +61,6 @@ setuptools.setup(
         'Topic :: System :: Networking',
     ),
     setup_requires=['cffi'],
-    cffi_modules=['build.py:ffibuilder'],
-    install_requires=['cffi'],
+    cffi_modules=['build_pynng.py:ffibuilder'],
 )
 
