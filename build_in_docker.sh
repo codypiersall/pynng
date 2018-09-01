14bef48f3fcd84a87534891f3ee565d718629dae
Cody Piersall <cody.piersall@gmail.com>
2018-08-31 21:37:20 -0500

    Let's do lots of building.

--- build_in_docker.sh
+++ build_in_docker.sh
@@ -1,13 +1,18 @@
 #! /bin/bash
 #
 # Meant to be called in Travis-ci, not really for anything else.
-#
-# example usage:
-# docker run --rm -i -t codypiersall/manylinux1-pynng
-python=/opt/python/cp35-cp35m/bin/python
-$python -m pip install .
-$python -m setup.py build
-$python -m setup.py build_ext --inplace
-$python -m pip install pytest
-$python -m pytest .
-
+for v in 34 35 36 37; do
+    python=/opt/python/cp${v}-cp${v}m/bin/python
+    $python -m pip install .
+    $python setup.py build
+    $python setup.py build_ext --inplace
+    $python setup.py bdist_wheel
+    $python -m pip install pytest
+    $python -m pytest .
+    rm pynng/*.so
+done
+$python setup.py sdist
+# rename wheels so they'll be officially compatible
+rename linux manylinux1 dist/*.whl
+# artifacts diretory should have been mounted under /mnt/artifacts
+mv dist /mnt/artifacts
