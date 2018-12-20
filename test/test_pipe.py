d98496237c83202edf5050f67ad566510aed5c29
Cody Piersall <cody.piersall@gmail.com>
2018-12-19 21:29:22 -0600

    Avoid hardcoding sleep times so CI doesn't fail.
    At least, hopefully CI won't fail.

--- test/test_pipe.py
+++ test/test_pipe.py
@@ -11,6 +11,18 @@ import pynng
 addr = 'tcp://127.0.0.1:31414'
 
 
+def wait_pipe_len(sock, expected, timeout=1):
+    """
+    Wait up to ``timeout`` seconds for the length of sock.pipes to become
+    ``expected`` value.  This prevents hardcodign sleep times
+    """
+    now = time.time()
+    later = now + timeout
+    while time.time() < later and len(sock.pipes) != expected:
+        time.sleep(0.001)
+    return len(sock.pipes) == expected
+
+
 def test_pipe_gets_added_and_removed():
     # add sleeps to ensure the nng_pipe_cb gets called.
     with pynng.Pair0(listen=addr, recv_timeout=1000) as s0, \
@@ -18,12 +30,10 @@ def test_pipe_gets_added_and_removed():
         assert len(s0.pipes) == 0
         assert len(s1.pipes) == 0
         s1.dial(addr)
-        time.sleep(0.05)
-        assert len(s0.pipes) == 1
-        assert len(s1.pipes) == 1
-    time.sleep(0.05)
-    assert len(s0.pipes) == 0
-    assert len(s1.pipes) == 0
+        assert wait_pipe_len(s0, 1)
+        assert wait_pipe_len(s1, 1)
+    assert wait_pipe_len(s0, 0)
+    assert wait_pipe_len(s1, 0)
 
 
 def test_close_pipe_works():
@@ -31,14 +41,14 @@ def test_close_pipe_works():
     with pynng.Pair0(listen=addr, recv_timeout=1000) as s0, \
             pynng.Pair0(dial=addr, recv_timeout=1000) as s1:
         time.sleep(0.03)
-        assert len(s0.pipes) == 1
-        assert len(s1.pipes) == 1
+        assert wait_pipe_len(s0, 1)
+        assert wait_pipe_len(s1, 1)
         pipe0 = s0.pipes[0]
         pipe1 = s1.pipes[0]
         assert pipe0.id != -1
         assert pipe1.id != -1
         pipe0.close()
         time.sleep(0.05)
-        assert len(s0.pipes) == 0
-        assert len(s1.pipes) == 0
+        assert wait_pipe_len(s0, 0)
+        assert wait_pipe_len(s1, 0)
 
